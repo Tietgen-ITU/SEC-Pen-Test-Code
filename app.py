11d02c6e9adc6af70e64a640b415c3d076848c71
Joachim Borup <73405008+JoachimBorup@users.noreply.github.com>
2022-11-09 14:03:28 +0100

    Replace password with hash and salt (#26)
    * Add click package to README
    
    * Swap order beteween derived_key and salt
    
    * Redefine users table to remove plaintext passwords
    
    * Fix bug with type error
    
    * Minor refactor of error handling
    
    * Update database

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import json, sqlite3, click, functools, os, hashlib,time, random, sys
-from flask import Flask, current_app, g, session, redirect, render_template, url_for, request
+
+import sqlite3, functools, os, time, random, sys
+from flask import Flask, session, redirect, render_template, url_for, request
+from password import hash_password, verify_password
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import subprocess
-
-
 
 ### DATABASE FUNCTIONS ###
 
@@ -16,33 +16,24 @@ def init_db():
     conn = connect_db()
     db = conn.cursor()
     db.executescript("""
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS notes;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS notes;
+        CREATE TABLE notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            assocUser INTEGER NOT NULL,
+            dateWritten DATETIME NOT NULL,
+            note TEXT NOT NULL,
+            publicID INTEGER NOT NULL
+        );
 
-CREATE TABLE notes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    assocUser INTEGER NOT NULL,
-    dateWritten DATETIME NOT NULL,
-    note TEXT NOT NULL,
-    publicID INTEGER NOT NULL
-);
-
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-);
- 
-INSERT INTO users VALUES(null,"admin", "password");
-INSERT INTO users VALUES(null,"bernardo", "omgMPC");
-INSERT INTO notes VALUES(null,2,"1993-09-23 10:10:10","hello my friend",1234567890);
-INSERT INTO notes VALUES(null,2,"1993-09-23 12:10:10","i want lunch pls",1234567891);
-
-""")
-#TODO: Change passwords
-#TODO: Change passwords from plaintext
-
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password_hash TEXT NOT NULL,
+            salt TEXT NOT NULL
+        );
+    """)
 
 ### APPLICATION SETUP ###
 app = Flask(__name__)
@@ -87,7 +78,7 @@ def notes():
             db = connect_db()
             c = db.cursor()
             statement = """SELECT * from NOTES where publicID = ?"""
-            c.execute(statement, noteid)
+            c.execute(statement, (noteid,))
             result = c.fetchall()
             if(len(result)>0):
                 row = result[0]
@@ -111,62 +102,68 @@ def notes():
 @app.route("/login/", methods=('GET', 'POST'))
 @limiter.limit("50 per hour")
 def login():
-    error = ""
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = connect_db()
         c = db.cursor()
-        statement = "SELECT * FROM users WHERE username = ? AND password = ?;"
-        c.execute(statement,(username, password))
+        statement = "SELECT * FROM users WHERE username = ?;"
+        c.execute(statement, (username,))
         result = c.fetchall()
-        print("Result", result)
-        #TODO: maybe do something here? write better logic 
-        if len(result) > 0:
-            session.clear()
-            session['logged_in'] = True
-            session['userid'] = result[0][0]
-            session['username']=result[0][1]
-            return redirect(url_for('index'))
-        else:
-            error = "Wrong username or password!"
-    return render_template('login.html',error=error)
+        
+        if len(result) < 1:
+            return render_template('login.html', error = "No such user!")
+
+        user = result[0]
+
+        if not verify_password(password, derived_key = user[2], salt = user[3]):
+            return render_template('login.html', error = "Incorrect password!")
+
+        session.clear()
+        session['logged_in'] = True
+        session['userid'] = user[0]
+        session['username'] = user[1]
+
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
 
 
 @app.route("/register/", methods=('GET', 'POST'))
 def register():
-    errored = False
-    usererror = ""
-    passworderror = ""
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        derived_key, salt = hash_password(request.form['password'])
+
         db = connect_db()
         c = db.cursor()
-        user_statement = """SELECT * FROM users WHERE username = ? ;"""
+        user_statement = """SELECT * FROM users WHERE username = ?;"""
 
-        c.execute(user_statement, username)
-        if(len(c.fetchall())>0):
-            errored = True
-            usererror = "That username is already in use by someone else!"
-        if(not errored):
-            statement = """INSERT INTO users(id,username,password) VALUES(null, ?, ?);"""
-            c.execute(statement,(username, password))
-            db.commit()
-            db.close()
-            return f"""<html>
-                        <head>
-                            <meta http-equiv="refresh" content="2;url=/" />
-                        </head>
-                        <body>
-                            <h1>SUCCESS!!! Redirecting in 2 seconds...</h1>
-                        </body>
-                        </html>
-                        """
-        
+        c.execute(user_statement, (username,))
+
+        if len(c.fetchall()) > 0:
+            return render_template('register.html', usererror = "That username is already in use by someone else!")
+
+        statement = """
+            INSERT INTO users(id, username, password_hash, salt)
+            VALUES(null, ?, ?, ?);
+        """
+        c.execute(statement, (username, derived_key, salt))
         db.commit()
         db.close()
-    return render_template('register.html',usererror=usererror,passworderror=passworderror)
+
+        return """
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="2;url=/" />
+                </head>
+                <body>
+                    <h1>SUCCESS!!! Redirecting in 2 seconds...</h1>
+                </body>
+            </html>
+        """
+
+    return render_template('register.html')
 
 
 @app.route("/search/", methods=('GET', 'POST'))
--- password.py
+++ password.py
@@ -4,7 +4,8 @@ from backports.pbkdf2 import pbkdf2_hmac
 
 def hash_password(password: str) -> tuple[str, str]:
     '''Hashes the given password using PBKDF2 with a random salt,
-    and then returns the salt and derived key as a tuple.'''
+    and then returns the tuple (derived_key, salt).'''
+
     # The salt should be generated with a cryptographically secure
     # pseudorandom number generator (CSPRNG), such as os.urandom()
     salt = hexlify(urandom(32))
@@ -12,10 +13,10 @@ def hash_password(password: str) -> tuple[str, str]:
     # Hashes the password using the SHA256 hash function, across 100.000 generations
     derived_key = hexlify(pbkdf2_hmac('SHA256', password.encode('UTF-8'), salt, 100000))
 
-    return salt, derived_key
+    return derived_key, salt
 
-def verify_password(password: str, salt: str, derived_key: str) -> bool:
-    '''Verifies the given password against the given salt and derived key.
+def verify_password(password: str, derived_key: str, salt: str) -> bool:
+    '''Verifies the given password against the given derived key and salt.
     Returns True if the password is correct, False otherwise.'''
 
     return derived_key == hexlify(pbkdf2_hmac('SHA256', password.encode('UTF-8'), salt, 100000))
